8fde4ba55cba21060eff31fb01f92915ec64e232
Mrunal <ghorpade.m@husky.neu.edu>
2019-04-14 13:06:44 -0400

    maze added

--- RobotController/src/com/neu/edu/RobotController.java
+++ RobotController/src/com/neu/edu/RobotController.java
@@ -1,4 +1,7 @@
+
 package com.neu.edu;
+
+import com.neu.edu.Maze;
 
 public class RobotController 
 {
@@ -6,5 +9,20 @@ public class RobotController
 	public static void main(String[] args)
 	{
 		
+		System.out.print("fsdfd");
+		
+		Maze maze = new Maze(new int[][] { 
+			{ 0, 0, 0, 0, 1, 0, 1, 3, 2 }, 
+			{ 1, 0, 1, 1, 1, 0, 1, 3, 1 },
+			{ 1, 0, 0, 1, 3, 3, 3, 3, 1 }, 
+			{ 3, 3, 3, 1, 3, 1, 1, 0, 1 }, 
+			{ 3, 1, 3, 3, 3, 1, 1, 0, 0 },
+			{ 3, 3, 1, 1, 1, 1, 0, 1, 1 }, 
+			{ 1, 3, 0, 1, 3, 3, 3, 3, 3 }, 
+			{ 0, 3, 1, 1, 3, 1, 0, 1, 3 },
+			{ 1, 3, 3, 3, 3, 1, 1, 1, 4 } 
+		});
+		
+		System.out.println("after maaze");
 	}
 }
